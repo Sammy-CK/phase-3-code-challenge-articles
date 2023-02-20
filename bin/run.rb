daca06b072790932afb4bb7d25b6e6d9df21c98d
Sammy-CK <sammycherono2696@gmail.com>
2023-02-20 10:57:24 +0300

    Labeled all created Instances

--- bin/run.rb
+++ bin/run.rb
@@ -2,24 +2,40 @@
 
 require_relative "../config/environment"
 
+#Initialize new Author instance
 sam = Author.new("Sammy Cherono")
 kevin = Author.new("Kevin Korir")
-
 allan = Author.new("Allan Aberi")
 phil = Author.new("Philip Mutisya")
 
-# puts sam.name, kevin.name,allan.name, phil.name
 
+puts "Author#name:\n", sam.name, kevin.name,allan.name, phil.name, "\n"
+
+#Initialize new Magazine instance
 forbes = Magazine.new("Forbes Magazine", "Wealth")
 architect = Magazine.new("Architect Digets", "Property")
 
-# puts forbes.name, forbes.category
-# forbes.name = "Forb Mag"
-# forbes.category = "Money"
-# puts forbes.name, forbes.category
+#name on Magazine
+puts "Magazine#name forbes: \n",forbes.name, "\n"
 
-# pp Magazine.all
+#category on Magazine
+puts "Magazine#category forbes: \n",forbes.category, "\n"
 
+
+pp "Before Magazine instance change:",forbes
+forbes.name = "Forb Mag"
+forbes.category = "Money"
+pp "After Magazine instance change:", forbes
+forbes.name = "Forbes Magazine"
+forbes.category = "Wealth"
+
+# Magazine. all
+puts "\n"
+pp "Magazine. all:", Magazine.all
+puts "\n"
+
+
+#new Article Instances
 mwalimu_sam = Article.new(sam, architect, "Mwalimu Sam")
 kevins_conner = Article.new(kevin, architect, "Kevin Conner")
 
@@ -29,29 +45,43 @@ mooney_sam = Article.new(sam, forbes, "Chase a bag")
 broke_sam = Article.new(sam, forbes, "Broke Boys")
 sam_pays = Article.new(sam, forbes, "Get rich quickk")
 
-# pp mwalimu_sam.author, mwalimu_sam.magazine
+puts "Article#title mwalimu_sam:", mwalimu_sam.title
+puts "\n"
+pp "Article. all:", Article.all
+puts "\n"
 
-# puts mwalimu_sam.title
-# pp Article.all
+# Article#author
+pp "Article#author mwalimu_sam:", mwalimu_sam.author
+puts "\n"
+# Article#magazine
+pp "Article#magazine mwalimu_sam:", mwalimu_sam.magazine
+puts "\n"
 
-# pp mwalimu_sam.author
+# Author#articles
+pp "Author#articles sam:", sam.articles
+puts "\n"
 
-# pp  mwalimu_sam.magazine
+# Author#magazines
+pp "Author#magazines sam:", sam.magazines
+puts "\n"
 
-# pp sam.articles
+# Magazine#contributors
+pp "Magazine#contributors forbes:", forbes.contributors
+puts "\n"
 
-# pp sam.magazines
+# Author#add_article(magazine, title)
+sam.add_article(forbes, "The bold and the rich")
 
-# pp forbes.contributors
-# pp architect.contributors
+# Author#topic_areas
+pp "Author#topic_areas sam:", sam.topic_areas
+puts "\n"
+# Magazine.find_by_name(name)
+pp "Magazine.find_by_name(name):", Magazine.find_by_name("Forbes Magazine")
+puts "\n"
 
-# sam.add_article(forbes, "The bold and the rich")
-# pp forbes.article_titles
+#Magazine#article_titles
+pp "Magazine#article_titles architect: ", architect.article_titles
+puts "\n"
 
-# # pp sam.topic_areas
-
-# pp Magazine.find_by_name("Forbes Magazine")
-
-# pp architect.article_titles
-
-# pp forbes.contributing_authors
+#Magazine#contributing_authors
+pp "Magazine#contributing_authors forbes:", forbes.contributing_authors
